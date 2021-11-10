2e498fcf2b25c9aeac528f3ede190ece5755cc2e
Flavio Dumitru Roman <58359247+flaaroman@users.noreply.github.com>
2021-11-10 12:59:10 +0100

    Update regole.pl

--- regole.pl
+++ regole.pl
@@ -8,8 +8,19 @@ trasforma(nord,pos(R,C),pos(NuovaRigaSopra,C)):- NuovaRigaSopra is R-1.
 trasforma(est,pos(R,C),pos(R,ColonnaADestra)):- ColonnaADestra is C+1.
 trasforma(sud,pos(R,C),pos(NuovaRigaSotto,C)):- NuovaRigaSotto is R+1.
 
-checkRiga:-
-    occupata(pos(1,1),1),
-    occupata(pos(1,2),2),
-    occupata(pos(1,3),3),
-    occupata(pos(1,4),4).
+posCorretta(pos(X,Y),Valore):-X = 1,Y = 1,Valore = 1.
+posCorretta(pos(X,Y),Valore):-X = 1,Y = 2,Valore = 2.
+posCorretta(pos(X,Y),Valore):-X = 1,Y = 3,Valore = 3.
+posCorretta(pos(X,Y),Valore):-X = 1,Y = 4,Valore = 4.
+posCorretta(pos(X,Y),Valore):-X = 2,Y = 1,Valore = 5.
+posCorretta(pos(X,Y),Valore):-X = 2,Y = 2,Valore = 6.
+posCorretta(pos(X,Y),Valore):-X = 2,Y = 3,Valore = 7.
+posCorretta(pos(X,Y),Valore):-X = 2,Y = 4,Valore = 8.
+posCorretta(pos(X,Y),Valore):-X = 3,Y = 1,Valore = 9.
+posCorretta(pos(X,Y),Valore):-X = 3,Y = 2,Valore = 10.
+posCorretta(pos(X,Y),Valore):-X = 3,Y = 3,Valore = 11.
+posCorretta(pos(X,Y),Valore):-X = 3,Y = 4,Valore = 12.
+posCorretta(pos(X,Y),Valore):-X = 4,Y = 1,Valore = 13.
+posCorretta(pos(X,Y),Valore):-X = 4,Y = 2,Valore = 14.
+posCorretta(pos(X,Y),Valore):-X = 4,Y = 3,Valore = 15.
+posCorretta(pos(X,Y),Valore):-X = 4,Y = 4,Valore = 0.
